719aab014b3d5c60de8ca8407c64ac5386ae5a6c
Carlos Rodriguez <carlos.a.rodriguez100@gmail.com>
2019-04-10 21:55:12 -0400

    adds quotes and quotes grid components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="container">
-    
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue'
     export default {
+        data : function(){
+            return {
+                quotes: ['Our first quote','Our second quote'],
+                maxQuotes : 10
+            }
+        },
+        components: {
+            appQuoteGrid : QuoteGrid
+        }
         
     }
 </script>
